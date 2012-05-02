44a948c46e711cf0a5848b861923a87b9f7784fd
Phil Meckel <philmeckel@gmail.com>
2012-05-02 19:16:28 -0400

    Added output database argument to volusionxml2mysql script

--- export_volusion_xml.sh
+++ export_volusion_xml.sh
@@ -4,4 +4,5 @@ java -jar selenium-server-standalone-2.21.0.jar [-timeout 3600] &
 sleep 5
 ruby scripts/getFilenames.rb
 ruby scripts/fetchFiles.rb
-ruby scripts/volusionxml2mysql.rb
+ruby scripts/volusionxml2mysql.rb mbk_volusion_export
+
