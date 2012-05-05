615d105c515b11924fee74277d436a7b694a3435
Jason Harab <jason@ubuntu.(none)>
2012-05-05 18:45:34 -0400

    Get Column Headers and Data Type
    Modified getFilenames script to create a new file that has:
    	<column name>,<datatype>

--- scripts/mbk_params.rb
+++ scripts/mbk_params.rb
@@ -1,11 +1,11 @@
 #Add the following env variables to your .bashrc and 
 #set them with the appropriate values...or hardcode them in 
 
-MBK_VOLUSION_URL        = ENV["MBK_VOLUSION_URL"]
-MBK_VOLUSION_USER       = ENV["MBK_VOLUSION_USER"]
-MBK_VOLUSION_PASS       = ENV["MBK_VOLUSION_PASS"]
-MBK_VOLUSION_OUTPUT_DIR = ENV["MBK_VOLUSION_OUTPUT_DIR"]
+MBK_VOLUSION_URL        = "https://www.modeltrainstuff.com"
+MBK_VOLUSION_USER       = "philz@modeltrainstuff.com"
+MBK_VOLUSION_PASS       = "voodoo55"
+MBK_VOLUSION_OUTPUT_DIR = "/home/jason/MBK/fetched_xml"
 
-MBK_DB_HOST             = ENV["MBK_DB_HOST"]
-MBK_DB_USER             = ENV["MBK_DB_USER"]
-MBK_DB_PASS             = ENV["MBK_DB_PASS"]
+MBK_DB_HOST             = "remote.modeltrainstuff.com"
+MBK_DB_USER             = "philz"
+MBK_DB_PASS             = "asdyuh23"
