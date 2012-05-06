1c13118e7dbce8ca0af655ab69f9b93fc7172b5f
Phil Meckel <philmeckel@gmail.com>
2012-05-05 21:50:43 -0400

    Added parameter to script or output database

--- export_volusion_xml.sh
+++ export_volusion_xml.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+#check for the output database name parameter
+if [ $# -ne 2 ]
+then
+  echo "Usage: `$0` {output_database_name}"
+  exit
+fi
+
+rm -rf volusion_exported_xml
+rm filesToDownload
 java -jar selenium-server-standalone-2.21.0.jar [-timeout 3600] &
 sleep 5
 ruby scripts/getFilenames.rb
 ruby scripts/fetchFiles.rb
-ruby scripts/volusionxml2mysql.rb mbk_volusion_export
+ruby scripts/volusionxml2mysql.rb $1
 
