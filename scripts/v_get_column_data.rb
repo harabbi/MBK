dfe33418facd11b41ee8b902f57a1aed2ae49501
Jason <j.harabbi@gmail.com>
2012-09-09 15:23:28 -0400

    Fixed v_get_column_data.rb for real this time...

--- scripts/v_get_column_data.rb
+++ scripts/v_get_column_data.rb
@@ -22,43 +22,43 @@ mbk_create_dir(coldir)
 
 $a.get('https://www.modeltrainstuff.com/admin/db_export.asp')
 
-columns = $a.page.search('#export_columns_row span table')
-columns.each{|c| columns.delete(c) if !c.nil? and (c.text.include? "Check All")}
-
 IO.readlines("#{Dir.pwd}/tablesToDownload").each do |table_name|
 	mbkloginfo(__FILE__, "Processing #{table_name.strip!}...")
   cf = File.open("#{coldir}/#{table_name}.sql", "w")
   s = "create table if not exists `#{table_name}` (\n"
   cnt = "1"
-	columns.find{|c| c.search('input').first.attribute('id').text == table_name.strip}.text.strip.split(")").each do |x|
-		if x.include? "Virtual Columns"
-			x.split(": ")[1].downcase!.split(" ").each do |virtual_column|
-        virtual_column.gsub!("*", "")
-				s << "`#{virtual_column}` text,\n"
-			end
-		else
-		  column, type = x.split(" (")
-		  next unless column and type
 
-		  column.gsub!(/^ /, "")
-		  column.downcase!
-		  column.gsub!("* ", "")
+  $a.page.search("#span_#{table_name} label").each do |x|
+		unless x.text.include? "Check All"
+		  column, type = x.text.split(" (") || x.text
 
-		  type.downcase!
-		  type.gsub!("* ", "")
-		  type.gsub!(" : ", "(").gsub!(/$/, ")") if type.include?(":")
-		  type.gsub!("text", "varchar")
-		  type.gsub!("memo", "text")
-		  type.gsub!("long", "bigint")
-		  type.gsub!("currency", "float")
-		  type.gsub!("varchar(-1)", "text")
+      if type.nil?
+        # virtual column
+        column.gsub!("*", "")
+        n = "`#{column}` text,\n"
 
-      if column.strip[-2..-1] == "id" and cnt == "1" and type.strip != "text"
-		    s << "`#{column.strip}` #{type.strip} PRIMARY KEY,\n" if column.strip[-2..-1] == "id" and cnt == "1"
       else
-        s << "`#{column.strip}` #{type.strip},\n"
+        column.gsub!(/^ /, "")
+        column.downcase!
+        column.gsub!("* ", "")
+
+        type.downcase!
+        type.gsub!("* ", "")
+        type.gsub!(" : ", "(") if type.include?(":")
+        type.gsub!("text", "varchar")
+        type.gsub!("memo", "text")
+        type.gsub!("long", "bigint")
+        type.gsub!("currency", "float")
+        type.gsub!("varchar(-1)", "text")
+
+        if column.strip[-2..-1] == "id" and cnt == "1" and type.strip != "text"
+          n = "`#{column.strip}` #{type.strip} PRIMARY KEY,\n" if column.strip[-2..-1] == "id" and cnt == "1"
+        else
+          n = "`#{column.strip}` #{type.strip},\n"
+        end
       end
       cnt.next!
+      s << n
 		end
 	end
 	s <<" `mbk_ready_to_import` BOOLEAN DEFAULT FALSE, `mbk_updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, `mbk_created_at` DATETIME DEFAULT NULL) ENGINE=MyISAM;"
