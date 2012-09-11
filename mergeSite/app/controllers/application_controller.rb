b42578d0fe0c4deeda53f0846119d77ed8afb527
Jason <j.harabbi@gmail.com>
2012-09-11 11:48:26 -0400

    Installed to_xls gem and made initial link for testing

--- mergeSite/app/controllers/application_controller.rb
+++ mergeSite/app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def home
-    @page = params[:page].to_i || 1
+    @page = (params[:page] || 1).to_i
     @all_products = Product.all.slice((100*(@page-1))...(100*@page))
     @all_customers = Customer.all.slice((100*(@page-1))...(100*@page))
+
+    respond_to do |format|
+      format.html
+      format.xls do
+        render :xls => @all_products, 
+                       :columns => Product.attribute_names, 
+                       :headers => Product.attribute_names.collect{|attr| attr.camelize}
+      end
+    end
   end
 end
